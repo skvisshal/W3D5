8fe786b7584d414ee36611abce03edb64496fac9
Visshal Suresh <skvisshal@gmail.com>
2021-10-22 11:22:13 -0400

    polyTree Phase 1 and 2

--- visshal_steven_poly_tree_project/lib/00_tree_node.rb
+++ visshal_steven_poly_tree_project/lib/00_tree_node.rb
@@ -1,3 +1,18 @@
 class PolyTreeNode
 
+    attr_reader :value, :parent, :children
+
+    def initialize(value)
+        @value = value
+        @parent = nil
+        @children = []
+    end
+
+    def parent=(node)
+        @parent.children.delete(self) if !@parent.nil?
+        @parent = node
+        node.children << self if !node.nil? && !node.children.include?(self)
+    end
+
+    
 end
