9c6554ab75318c5865a43e5cf01baff5403d77d9
Stevens <stevensookhai@gmail.com>
2021-10-22 11:38:12 -0400

    Completed ploy tree

--- visshal_steven_poly_tree_project/lib/00_tree_node.rb
+++ visshal_steven_poly_tree_project/lib/00_tree_node.rb
@@ -14,5 +14,35 @@ class PolyTreeNode
         node.children << self if !node.nil? && !node.children.include?(self)
     end
 
+    def add_child(child_node)
+        child_node.parent = self
+    end 
+
+    def remove_child(child_node)
+        raise 'not a child' if !@children.include?(child_node)
+        child_node.parent = nil
+    end 
+
+    def dfs(target_value)
+        return self if @value == target_value
+
+        @children.each do |child|
+            node = child.dfs(target_value)
+            return node if !node.nil? 
+        end 
+        nil 
+    end 
+
+
+    def bfs(target_value)
+        queue = [self]
+        while !queue.empty?
+            node = queue.shift
+            return node if node.value == target_value
+            queue += node.children
+        end 
+        nil
+    end 
+
     
 end
